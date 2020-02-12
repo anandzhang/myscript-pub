b19a5a3a6169f5c284523c17e17a88ccc991380e
anandzhang <anandzhangnew@gmail.com>
2020-02-12 15:57:48 +0800

    删除测试语句

--- deploy/aliyun-centos-deploy-nodejs-web.sh
+++ deploy/aliyun-centos-deploy-nodejs-web.sh
@@ -56,7 +56,6 @@ clone_git_repository() {
 question_install_git='Do you need to install git to clone the project you need to deploy？ (Y/n): '
 while true; do
     read -p "$question_install_git" flag_install_git
-    echo "test output: $flag_clone_info"
     if [ "$flag_install_git" == 'y' -o "$flag_install_git" == 'Y' ]; then
         echo '## Installing git'
         install_git
